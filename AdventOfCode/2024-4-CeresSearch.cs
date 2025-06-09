dd31b477e22229c19156b38b5b5208a7446a3c21
David Jarzynka <178108294+dajarzynka@users.noreply.github.com>
2025-06-08 23:01:40 -0500

    Day 4 Solution. Off By One Error Corrections

--- AdventOfCode/2024-4-CeresSearch.cs
+++ AdventOfCode/2024-4-CeresSearch.cs
@@ -9,7 +9,8 @@ namespace AdventOfCode
     {
         internal static int Execute()
         {
-            var input = File.ReadAllLines("2024Inputs/2024Problem4test.txt");
+            var input = File.ReadAllLines("2024Inputs/2024Problem4.txt");
+            var dataSetLength = input.Length;
             var workingDataSet = new string[input.First().Length, input.Length];
             var result = 0;
 
@@ -30,21 +31,52 @@ namespace AdventOfCode
                     if (workingDataSet[i,j].Equals("X", StringComparison.OrdinalIgnoreCase))
                     {
                         //right
-                        result = CheckRight(i, j, workingDataSet);
+                        if (j<dataSetLength-3)
+                        {
+                            result += CheckRight(i, j, workingDataSet);
+                        }
 
                         //left
+                        if(j>2)
+                        {
+                            result += CheckLeft(i, j, workingDataSet);
+                        }
 
                         //down
+                        if(i<dataSetLength-3)
+                        {
+                            result += CheckDown(i, j, workingDataSet);
+                        }
 
                         //up
+                        if(i>2)
+                        {
+                            result += CheckUp(i, j, workingDataSet);
+                        }
 
                         //down + right
+                        if(i<dataSetLength-3 && j<dataSetLength-3)
+                        {
+                            result += CheckDownRight(i, j, workingDataSet);
+                        }
 
                         //down + left
+                        if(i<dataSetLength-3 && j>2)
+                        {
+                            result += CheckDownLeft(i, j, workingDataSet);
+                        }
 
                         //up + left
+                        if(i>2 && j>2)
+                        {
+                            result += CheckUpLeft(i, j, workingDataSet);
+                        }
 
                         //up + right
+                        if(i>2 && j<dataSetLength-3)
+                        {
+                            result += CheckUpRight(i, j, workingDataSet);
+                        }
                     }
                 }
             }
@@ -60,13 +92,13 @@ namespace AdventOfCode
             //    Console.WriteLine();
             //}
 
-            return 0;
+            return result;
            
         }
 
         private static int CheckRight(int i, int j, string[,] workingDataSet)
         {
-            if (!workingDataSet[i, j+1].Equals("M", StringComparison.OrdinalIgnoreCase))
+            if (!workingDataSet[i, j + 1].Equals("M", StringComparison.OrdinalIgnoreCase))
             {
                 return 0;
             }
@@ -75,6 +107,24 @@ namespace AdventOfCode
                 return 0;
             }
             if (!workingDataSet[i, j + 3].Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+
+        private static int CheckDownRight(int i, int j, string[,] workingDataSet)
+        {
+            if (!workingDataSet[i + 1, j + 1].Equals("M", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i + 2, j + 2].Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i + 3, j + 3].Equals("S", StringComparison.OrdinalIgnoreCase))
             {
                 return 0;
             }
@@ -100,17 +150,17 @@ namespace AdventOfCode
             return 1;
         }
 
-        private static int CheckUp(int i, int j, string[,] workingDataSet)
+        private static int CheckDownLeft(int i, int j, string[,] workingDataSet)
         {
-            if (!workingDataSet[i+1, j].Equals("M", StringComparison.OrdinalIgnoreCase))
+            if (!workingDataSet[i + 1, j - 1].Equals("M", StringComparison.OrdinalIgnoreCase))
             {
                 return 0;
             }
-            if (!workingDataSet[i+2, j].Equals("A", StringComparison.OrdinalIgnoreCase))
+            if (!workingDataSet[i + 2, j - 2].Equals("A", StringComparison.OrdinalIgnoreCase))
             {
                 return 0;
             }
-            if (!workingDataSet[i+3, j].Equals("S", StringComparison.OrdinalIgnoreCase))
+            if (!workingDataSet[i + 3, j - 3].Equals("S", StringComparison.OrdinalIgnoreCase))
             {
                 return 0;
             }
@@ -118,5 +168,75 @@ namespace AdventOfCode
             return 1;
         }
 
+        private static int CheckDown(int i, int j, string[,] workingDataSet)
+        {
+            if (!workingDataSet[i + 1, j].Equals("M", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i + 2, j].Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i + 3, j].Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+
+        private static int CheckUp(int i, int j, string[,] workingDataSet)
+        {
+            if (!workingDataSet[i - 1, j].Equals("M", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 2, j].Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 3, j].Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+        private static int CheckUpRight(int i, int j, string[,] workingDataSet)
+        {
+            if (!workingDataSet[i - 1, j + 1].Equals("M", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 2, j + 2].Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 3, j + 3].Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+
+        private static int CheckUpLeft(int i, int j, string[,] workingDataSet)
+        {
+            if (!workingDataSet[i - 1, j - 1].Equals("M", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 2, j - 2].Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            if (!workingDataSet[i - 3, j - 3].Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            return 1;
+        }
     }
 }
